3e655a51423878ea9f9b45be17f21cec644e57f7
童杰 <tonge@tongjiedeMacBook-Pro.local>
2019-04-15 23:33:09 +0800

    test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,95 +1,113 @@
 import { Component } from '@angular/core';
-import { NavController,ModalController,AlertController } from 'ionic-angular';
-import {PrintProvider} from '../../providers/print/print';
-import {PrinterListModalPage} from '../printer-list-modal/printer-list-modal';
+import { NavController, ModalController, AlertController } from 'ionic-angular';
+import { PrintProvider } from '../../providers/print/print';
+import { PrinterListModalPage } from '../printer-list-modal/printer-list-modal';
 
+declare var CordovaXprinter;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  selectedPrinter:any=[];
+  selectedPrinter: any = [];
+  printerList: any = [];
+  devices: any = [];
 
-  constructor(public navCtrl: NavController,private modalCtrl:ModalController,
-    private printProvider:PrintProvider,
-    private alertCtrl:AlertController) {
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController,
+    private printProvider: PrintProvider,
+    private alertCtrl: AlertController) {
 
   }
 
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad BluetoothPage');
 
-  listBTDevice()
-  {
-    this.printProvider.searchBt().then(datalist=>{
-      
-      //1. Open printer select modal
-      let abc=this.modalCtrl.create(PrinterListModalPage,{data:datalist});
-      
-      //2. Printer selected, save into this.selectedPrinter
-      abc.onDidDismiss(data=>{
-        this.selectedPrinter=data;
+  }
 
-        let xyz=this.alertCtrl.create({
-          title: data.name+" selected",
-          buttons:['Dismiss']
-        });
-        xyz.present();
+  scan() {
 
-      });
-      
-      //0. Present Modal
-      abc.present();
-
-    },err=>{
-      console.log("ERROR",err);
-      let mno=this.alertCtrl.create({
-        title:"ERROR "+err,
-        buttons:['Dismiss']
-      });
-      mno.present();
+    this.printProvider.searchBt().then(datalist => {
+      this.printerList = datalist;
+    }, err => {
+      alert(err);
     })
 
+
+    CordovaXprinter.scanDevice({}, (data) => {
+      this.devices = data;
+      console.log(JSON.stringify(data));
+    }, err => { console.log(err) })
   }
 
-  testConnectPrinter()
-  {
-    var id=this.selectedPrinter.id;
-    if(id==null||id==""||id==undefined)
-    {
+  selectDevice(item) {
+    console.log("连接设备中")
+    CordovaXprinter.scanDevice(item, (data) => {
+      console.log("连接设备成功" + data)
+
+      // CordovaXprinter.printThreeData(
+      //   { left: "xxx", middle: "xxx", right: "xxx" },
+      //   (succ) => {
+      //     console.log('打印成功' + succ)
+      //   }, (err) => {
+      //     console.log('打印失败' + err)
+
+      //   });
+      CordovaXprinter.writeDevice({
+        storeName: 'xx',
+        buyerStoreName: 'xx',
+        mobile: '188',
+        provinceName: '',
+        cityName:'',
+        districtName:'',
+        address:'',
+        orderNo: '',
+        orderType: '',
+        shippingStatus: '',
+        orderDetailList: [{
+          goodsSn: '',
+          goodsNumber: '',
+          unit: '',
+          goodsPrice: '',
+          totalFee: ''
+        }],
+        totalFee: '',
+        discount: '',
+        orderAmount:1,
+        payCode: '',
+        payStatus: '',
+
+      },
+        (succ) => {
+          console.log('打印成功' + succ)
+        }, (err) => {
+          console.log('打印失败' + err)
+        });
+    }, err => { console.log(err) })
+  }
+
+  select(item) {
+    this.selectedPrinter = item;
+    var id = this.selectedPrinter.id;
+    if (id == null || id == "" || id == undefined) {
       //nothing happens, you can put an alert here saying no printer selected
     }
-    else
-    {
-      let foo=this.printProvider.connectBT(id).subscribe(data=>{
-        console.log("CONNECT SUCCESSFUL",data);
-
-        let mno=this.alertCtrl.create({
-          title:"Connect successful",
-          buttons:['Dismiss']
-        });
-        mno.present();
-
-      },err=>{
-        console.log("Not able to connect",err);
-        let mno=this.alertCtrl.create({
-          title:"ERROR "+err,
-          buttons:['Dismiss']
-        });
-        mno.present();
+    else {
+      let foo = this.printProvider.connectBT(id).subscribe(data => {
+        console.log("CONNECT SUCCESSFUL", data);
+      }, err => {
+        console.log("Not able to connect", err);
       });
     }
   }
 
-  testPrinter()
-  {
-    var id=this.selectedPrinter.id;
-    if(id==null||id==""||id==undefined)
-    {
+  testPrinter() {
+    var id = this.selectedPrinter.id;
+    if (id == null || id == "" || id == undefined) {
       //nothing happens, you can put an alert here saying no printer selected
     }
-    else
-    {
-      let foo=this.printProvider.testPrint(id);
+    else {
+      let foo = this.printProvider.testPrint(id);
     }
   }
 
--- src/providers/print/print.ts
+++ src/providers/print/print.ts
@@ -1,58 +1,108 @@
 import { Injectable } from '@angular/core';
-import {AlertController} from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 
 @Injectable()
 export class PrintProvider {
 
-  constructor(private btSerial:BluetoothSerial,private alertCtrl:AlertController) {
-    
+  constructor(private btSerial: BluetoothSerial, private alertCtrl: AlertController) {
+
   }
 
-  searchBt()
-  {
+  searchBt() {
     return this.btSerial.list();
   }
 
-  connectBT(address)
-  {
+  connectBT(address) {
     return this.btSerial.connect(address);
 
   }
 
-  testPrint(address)
-  {
-    let printData="Test hello this is a test \n\n\n\n Hello Test 123 123 123\n\n\n"
+  testPrint(address) {
+    // let printData = `销售单 \n
+    // 单据编号：201904150954001\n
+    // 日期：2019年04月15日 09:54:21 \n
+    // -----------------------------------------------\n
+    // 品名\r\t数量\r\t单价\r\t金额\r\t单位 \n
+    // 商品1\r\t2\r\t20\r\t40\r\t个 \n
+    // 商品2\r\t2\r\t20\r\t40\r\t个 \n
+    // 商品3\r\t2\r\t20\r\t40\r\t个 \n
+    // -----------------------------------------------\n
+    // 合计\r\t 数量：6\r\t 金额\r\t 120.0元 \n
+    // -----------------------------------------------\n
+    // 备注： \n\n\n
+    // `;
+    //  let printData =[186, 220, 222];
 
-    
-    let xyz=this.connectBT(address).subscribe(data=>{
-      this.btSerial.write(printData).then(dataz=>{
-        console.log("WRITE SUCCESS",dataz);
+    // let printData = new Uint8Array(4);
+    // printData[0] = 0x41;
+    // printData[1] = 0x42;
+    // printData[2] = 0x43;
+    // printData[3] = 0x44;
 
-        let mno=this.alertCtrl.create({
-          title:"Print SUCCESS!",
-          buttons:['Dismiss']
+    var printData = `你好 \n 你好 \n 你好 \n`
+    var finalReceipt = `“Welcome to” + ‘\x20’ + companyName + ‘\x0a’ + “Date:” + ‘\x20’ + this.datetime
++ ‘\x0a’ + “Cashier:” + ‘\x20’ + cashierName + ‘\x0a’ + ‘\x0a’ + receiptItems + ‘\x0a’ + “Subtotal:” + ‘\x20’ + “$” + this.totalPayment + ‘\x0a’
++ “GST:” + ‘\x20’ + “$” + gst + ‘\x0a’ + “Service Charge:” + ‘\x20’ + “$” + svc + ‘\x0a’
++ “Total:”+ ‘\x20’ + “$” + val + ‘\x0a’ + “Amount Received:” + ‘\x20’ + “$” + this.amountReceived + ‘\x0a’ + “Change:”
++ ‘\x20’ + “$” + this.change + ‘\x0a’ + “Payment Type:” + ‘\x20’ + this.paymentType + ‘\x0a’ + footer + ‘\x0a’+ ‘\x0a’`
+    let xyz = this.connectBT(address).subscribe(data => {
+      this.btSerial.write(finalReceipt).then(dataz => {
+        console.log("WRITE SUCCESS", dataz);
+
+        let mno = this.alertCtrl.create({
+          title: "打印中,请稍后...",
+          buttons: ['好的']
         });
         mno.present();
 
         xyz.unsubscribe();
-      },errx=>{
-        console.log("WRITE FAILED",errx);
-        let mno=this.alertCtrl.create({
-          title:"ERROR "+errx,
-          buttons:['Dismiss']
+      }, errx => {
+        console.log("WRITE FAILED", errx);
+        let mno = this.alertCtrl.create({
+          title: "ERROR " + errx,
+          buttons: ['Dismiss']
         });
         mno.present();
       });
-      },err=>{
-        console.log("CONNECTION ERROR",err);
-        let mno=this.alertCtrl.create({
-          title:"ERROR "+err,
-          buttons:['Dismiss']
-        });
-        mno.present();
+    }, err => {
+      console.log("CONNECTION ERROR", err);
+      let mno = this.alertCtrl.create({
+        title: "ERROR " + err,
+        buttons: ['Dismiss']
       });
+      mno.present();
+    });
 
   }
+
+  stringToByte(str) {
+    var bytes = new Array();
+    var len, c;
+    len = str.length;
+    for (var i = 0; i < len; i++) {
+      c = str.charCodeAt(i);
+      if (c >= 0x010000 && c <= 0x10FFFF) {
+        bytes.push(((c >> 18) & 0x07) | 0xF0);
+        bytes.push(((c >> 12) & 0x3F) | 0x80);
+        bytes.push(((c >> 6) & 0x3F) | 0x80);
+        bytes.push((c & 0x3F) | 0x80);
+      } else if (c >= 0x000800 && c <= 0x00FFFF) {
+        bytes.push(((c >> 12) & 0x0F) | 0xE0);
+        bytes.push(((c >> 6) & 0x3F) | 0x80);
+        bytes.push((c & 0x3F) | 0x80);
+      } else if (c >= 0x000080 && c <= 0x0007FF) {
+        bytes.push(((c >> 6) & 0x1F) | 0xC0);
+        bytes.push((c & 0x3F) | 0x80);
+      } else {
+        bytes.push(c & 0xFF);
+      }
+    }
+    return bytes;
+
+
+  }
+
+
 
 }
