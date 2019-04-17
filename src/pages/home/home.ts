29b6ffa7c9d912e6770970da8092b842ce50407c
Tonge <tonge@live.cn>
2019-04-17 18:25:48 +0800

    add plugins

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -35,17 +35,27 @@ export class HomePage {
       console.log("Not able to connect", err);
     });
   }
-  success() {
-
+  success(data) {
+    console.log('success:' + data)
   }
-  failure() {
-
+  failure(data) {
+    console.log('failure:' + data)
   }
   testPrinter(tag) {
-
     if (tag == 0) {
-      this.bluetoothSerial.write('hello world hello world').then(this.success, this.failure);
+      console.log('hello world')
+      this.bluetoothSerial.write('hello world hello world hello world hello world').then(this.success, this.failure);
     } else if (tag == 1) {
+      console.log('[186, 220, 222]')
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
+      this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
       this.bluetoothSerial.write([186, 220, 222]).then(this.success, this.failure);
     } else if (tag == 2) {
       var data = new Uint8Array(4);
@@ -53,8 +63,10 @@ export class HomePage {
       data[1] = 0x42;
       data[2] = 0x43;
       data[3] = 0x44;
+      console.log(data)
       this.bluetoothSerial.write(data).then(this.success, this.failure);
     } else if (tag == 3) {
+      console.log(data.buffer)
       this.bluetoothSerial.write(data.buffer).then(this.success, this.failure);
     } else if (tag == 4) {
       var printData = new Uint8Array(3);
@@ -62,15 +74,45 @@ export class HomePage {
       printData[1] = 0x52;
       printData[2] = 0x0F;
       this.bluetoothSerial.write(printData).then(this.success, this.failure);
+      // var printData1 = [28, 38]
+      // this.bluetoothSerial.write(printData1).then(this.success, this.failure);
     } else if (tag == 5) {
-      this.bluetoothSerial.write('中文测试中文测试中文测试').then(this.success, this.failure);
+      console.log('中文测试中文测试中文测试')
+      //执行了这个之后，下面那个send也会乱码
+      this.bluetoothSerial.write('中文测试中文测试中文测试中文测试中文测试中文测试').then(this.success, this.failure);
+      // this.bluetoothSerial.write(this.stringToBytes('中文测试中文测试中文测试中文测试中文测试中文测试')).then(this.success, this.failure);
+    }
+    else if (tag == 6) {
+      console.log('send中文测试中文测试中文测试')
+      this.bluetoothSerial.send('中文测试中文测试中文测试中文测试中文测试中文测试').then(this.success, this.failure);
     }
 
   }
 
-
-
-  stringToByte(str) {
+  byteToString(byte) {
+    if (typeof byte === 'string') {
+      return byte;
+    }
+    var str = '',
+      _arr = byte;
+    for (var i = 0; i < _arr.length; i++) {
+      var one = _arr[i].toString(2),
+        v = one.match(/^1+?(?=0)/);
+      if (v && one.length == 8) {
+        var bytesLength = v[0].length;
+        var store = _arr[i].toString(2).slice(7 - bytesLength);
+        for (var st = 1; st < bytesLength; st++) {
+          store += _arr[st + i].toString(2).slice(2);
+        }
+        str += String.fromCharCode(parseInt(store, 2));
+        i += bytesLength - 1;
+      } else {
+        str += String.fromCharCode(_arr[i]);
+      }
+    }
+    return str;
+  }
+  stringToBytes(str) {
     var bytes = new Array();
     var len, c;
     len = str.length;
@@ -94,5 +136,6 @@ export class HomePage {
     }
     return bytes;
   }
+
 
 }
